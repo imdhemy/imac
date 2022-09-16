0dbda84769e39c131e6f335e3d125c460080008b
imdhemy <imdhemy@gmail.com>
2022-09-17 00:54:35 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# TODO: Generate a brewfile
+
+# Upgrade Latest MacOS updates and show progress
+sudo softwareupdate -i --verbose
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+# Install github desktop
+brew install github --cask
+
+# Install Fira Code font
+brew tap homebrew/cask-fonts
+brew install font-fira-code
+
+# Install Docker desktop for Mac
+brew install docker --cask

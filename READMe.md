9eca7b2cb5259f848cd62899b47b2fa430c1d6c7
Mohamad Eldhemy <imdhemy@gmail.com>
2022-09-21 13:26:48 +0200

    create brewfile

--- READMe.md
+++ READMe.md
@@ -1,3 +1,17 @@
 # IMac
 
 Prepares a Mac for development.
+
+## Usage
+
+Run the following command:
+
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/imdhemy/imac/main/script.sh)"
+
+```
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more
+information.
--- script.sh
+++ script.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
-
-# TODO: Generate a brewfile
+#!/bin/bash
 
 # Upgrade Latest MacOS updates and show progress
 sudo softwareupdate -i -a --verbose
@@ -8,25 +6,5 @@ sudo softwareupdate -i -a --verbose
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
-# Install freetds
-brew install freetds
-
-# Install PHP, Composer, and Xdebug
-brew install php
-brew install composer
-pecl install xdebug
-
-# Install github desktop
-brew install github --cask
-
-# Install Fira Code font
-brew tap homebrew/cask-fonts
-brew install font-fira-code
-
-# Install Docker Desktop if OS is 12.0 or higher
-if [[ $(sw_vers -productVersion | cut -d '.' -f 2) -ge 12 ]]; then
-    brew install docker --cask
-fi
-
-# Install Spectacle window manager
-brew install spectacle --cask
+# Install Homebrew Bundle and Cask from Brewfile
+brew bundle --file=./Brewfile

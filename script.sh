4ad81e2919091ddaef6d9dcc1f33d1f82c1fda49
Mohamad Eldhemy <imdhemy@gmail.com>
2022-09-24 17:51:56 +0200

    generate GPG key and add to github

--- script.sh
+++ script.sh
@@ -4,13 +4,31 @@
 sudo softwareupdate -i -a --verbose
 
 # Install Homebrew if not installed
-if test ! $(which brew); then
-  echo "Installing Homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+Homebrew=$(which brew)
+if [ -z "$Homebrew" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 # Install Homebrew Bundle and Cask from Brewfile
 brew bundle --file=./Brewfile
 
-# Install LTS Node.js
-sudo n lts
+# Install LTS Node.js if not installed
+Node=$(which node)
+if [ -z "$Node" ]; then
+    echo "Installing LTS Node.js..."
+    nvm install --lts
+fi
+
+# Generate GPG Key
+gpg --default-new-key-algo rsa4096 --gen-key
+
+# Get ID of GPG Key
+GPGID=$(gpg --list-secret-keys --keyid-format LONG | grep sec | cut -d'/' -f2 | cut -d' ' -f1)
+
+# Copy the GPG Key ID in ASCII armor format to clipboard
+gpg --armor --export "$GPGID" | pbcopy
+
+echo "Key copied to clipboard. Paste it into GitHub."
+
+# Open browser to GitHub GPG Key page
+open https://github.com/settings/gpg/new

1b040afbbf9e241831076dcf7787424ec46d5aaf
Mohamad Eldhemy <imdhemy@gmail.com>
2022-09-24 17:20:00 +0200

    install nodeJs, npm and yarn

--- script.sh
+++ script.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Upgrade Latest MacOS updates and show progress
 sudo softwareupdate -i -a --verbose
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Install Homebrew if not installed
+if test ! $(which brew); then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
 
 # Install Homebrew Bundle and Cask from Brewfile
 brew bundle --file=./Brewfile
+
+# Install LTS Node.js
+sudo n lts
